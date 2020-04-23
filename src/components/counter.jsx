963274ce684ca2c44d4e8a580cd37e51412e216e
Timothy <timothyattah733@gmail.com>
2020-04-23 19:27:32 +0100

    added components folder

---
+++ src/components/counter.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+	render() {
+		return (
+			<div>
+				<h1>React App</h1>
+				<div>
+					<span>1</span>
+					<button>Increment</button>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './components/counter';
 
 const element = <h1>Hello world!!!!!!!</h1>;
 
 const root = document.getElementById('root');
-ReactDOM.render(element, root);
+ReactDOM.render(<Counter />, root);
