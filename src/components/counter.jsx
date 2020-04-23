6301137eaa64196a1c22ffbc5931ae94203684cf
Timothy <timothyattah733@gmail.com>
2020-04-23 21:56:38 +0100

    added funtionality to a button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 class Counter extends Component {
+	state = {
+		count: 0,
+	};
+
+	incrementCount = () => {
+		this.setState({ count: this.state.count + 1 });
+	};
+
 	render() {
 		return (
 			<div>
 				<h1>React App</h1>
 				<div>
-					<span>1</span>
-					<button>Increment</button>
+					<span className={this.getColorClasses()}>{this.formatCount()}</span>
+					<button onClick={this.incrementCount} className="btn-plus">
+						Increment
+					</button>
 				</div>
 			</div>
 		);
+	}
+
+	getColorClasses() {
+		let classes = 'zero-';
+		classes += this.state.count === 0 ? 'primary' : 'warning';
+		return classes;
+	}
+
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? 'Zero' : count;
 	}
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './components/counter.css';
 import Counter from './components/counter';
 
-const element = <h1>Hello world!!!!!!!</h1>;
+//const element = <h1>Hello world!!!!!!!</h1>;
 
 const root = document.getElementById('root');
 ReactDOM.render(<Counter />, root);
